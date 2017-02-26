0d6ece5c117df4eb2d544daaff6ce0c7bea8f26b
Justin Gualtieri <JustinGualtieri@Justins-MacBook-Pro-7.local>
2017-02-25 19:00:12 -0500

    Began work on the Prompt Activity

---
+++ Amina/app/src/main/java/edu/dartmouth/cs/jgualtieri/amina/PromptActivity/PromptActivity.java
@@ -0,0 +1,24 @@
+package edu.dartmouth.cs.jgualtieri.amina.PromptActivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.dartmouth.cs.jgualtieri.amina.R;
+
+public class PromptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
